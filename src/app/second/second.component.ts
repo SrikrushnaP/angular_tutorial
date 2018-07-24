da1104dc77235ef0681656d358cf62911962371a
Srikrushna Pal <Srikrushna Pal>
2018-07-24 13:28:03 +0530

    Template Reference variable, one way binding and two way binding

--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -3,17 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-second',
   template: `
-    <p>
-      second works!
-    </p>
+    /* hdhd */
+    <input #myInput type="text" />
+    <button (click)="trvFunc(myInput.value)">Template Ref Var</button>
+    {{ input1 }}
+
+    <br><br>
+    <button (click)="greatFun()">One way (DS to View)</button>
+    <h3 [style.color]="hiLightColor">One-way: from data source to view target</h3>
+
+    <br /><br />
+
+    Two Way Binding: <input type="text" [(ngModel)]="myName"/> {{ myName }}
+
   `,
   styles: []
 })
 export class SecondComponent implements OnInit {
 
+  public input1 = "";
+
+  public oneWayBind = "One-way: from view target to data source";
+  public hiLightColor = "green";
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  trvFunc(value){
+    this.input1 = value;
+  }
+
+  greatFun(){
+    console.log(this.oneWayBind);
   }
 
 }
