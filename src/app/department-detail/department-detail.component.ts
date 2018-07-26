3bc73f6140ff41e0c78c936d59f2e589097633bf
Srikrushna Pal <Srikrushna Pal>
2018-07-26 15:17:40 +0530

    Route parameter, drawback of snapshoot approch and paramap observable

--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from  '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from  '@angular/router';
 
 @Component({
   selector: 'app-department-detail',
   template: `
     <h3>You click on department whose ID is {{ departmentId }}</h3>
+    <div>
+      <button (click)="goPrevious()">Previous</button>
+      <button (click)="goNext()">Next</button>
+    </div>
   `,
   styles: []
 })
@@ -12,11 +16,26 @@ export class DepartmentDetailComponent implements OnInit {
 
   public departmentId;
 
-  constructor( private route: ActivatedRoute) { }
+  constructor( private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
-    let id = parseInt(this.route.snapshot.paramMap.get('id'));
-    this.departmentId = id;
+    // Snapshoot approch
+    // let id = parseInt(this.route.snapshot.paramMap.get('id'));
+    // this.departmentId = id;
+
+    // paramap observable
+    this.route.paramMap.subscribe((params: ParamMap)=>{
+      let id = parseInt(params.get('id'));
+      this.departmentId = id;
+    });
   }
 
+  goPrevious(){
+    let prevId = this.departmentId - 1;
+    this.router.navigate(['/departments', prevId]);
+  }
+  goNext(){
+    let nextId = this.departmentId + 1;
+    this.router.navigate(['/departments', nextId]);
+  }
 }
